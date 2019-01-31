2e9a2eac7c6c5894f9dcea9800eb63d5ce1a18f8
Gyorgy Kocsis <gyorgykocsisestrea@gmail.com>
2019-01-31 22:03:30 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kocsis György on 2019. 01. 31..
+//  Copyright © 2019. Jolakotturinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
